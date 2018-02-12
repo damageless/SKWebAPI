fd07e07c97a396db9e5228e22d344f7c97a30771
Adam Gessel <damageless@gmail.com>
2018-02-11 21:33:45 -0600

    Update Endpoint.swift

--- Sources/Endpoint.swift
+++ Sources/Endpoint.swift
@@ -37,6 +37,7 @@ public enum Endpoint: String {
     case chatPostMessage = "chat.postMessage"
     case chatMeMessage = "chat.meMessage"
     case chatUpdate = "chat.update"
+    case chatPostEphemeral = "chat.PostEphemeral"
     case dndInfo = "dnd.info"
     case dndTeamInfo = "dnd.teamInfo"
     case emojiList = "emoji.list"
